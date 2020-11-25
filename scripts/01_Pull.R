9d877f9cbc5f62394d512609b5feb644c583e450
froebbm <64562062+froebbm@users.noreply.github.com>
2020-11-24 22:31:44 -0500

    initial push

--- scripts/01_Pull.R
+++ scripts/01_Pull.R
@@ -1,6 +1,4 @@
 #### SetUp ####
-dir = "C:/Users/brian/OneDrive/Documents/GitHub/WakeCounty_Affordable_Housing"
-setwd(dir)
 source('./scripts/SetUp.R', echo = TRUE)
 #### Functions ####
 url_shp_to_sf <- function(URL) {
@@ -17,27 +15,42 @@ url_shp_to_sf <- function(URL) {
   
   shp <- dir(tempdir(), "*.shp$")
   lyr <- sub(".shp$", "", shp)
-  y <- read_sf(shp, quiet = TRUE)
+  
+  if(length(shp) > 1){
+    y <- read_sf(shp[1], quiet = TRUE)
+  } else {
+    y <- read_sf(shp, quiet = TRUE)
+    
+  }
 
   unlink(dir(td))
   setwd(wd)
   return(y)
 }
 #### Data Pulls ####
-Parcel_URL = paste0('ftp://wakeftp.co.wake.nc.us/gis/Webdownloads/',
+parcel_URL = paste0('ftp://wakeftp.co.wake.nc.us/gis/Webdownloads/',
                     'SHAPEFILES/Wake_Property_2020_10.zip')
-Parcels  = url_shp_to_spdf(Parcel_URL)
-Parcels  = st_make_valid(Parcels)
-Parcels  = subset(Parcels, st_is_valid(Parcels))
+parcels  = url_shp_to_spdf(parcel_URL)
+parcels  = st_make_valid(parcels)
+parcels  = subset(parcels, st_is_valid(parcels))
 
 out_path = "./data/Parcels_October_2020"
-write_sf(Parcels, paste0(out_path, '.shp'))
-write_rds(Parcels, paste0(out_path, '.rds'))
+write_sf(parcels, paste0(out_path, '.shp'))
+write_rds(parcels, paste0(out_path, '.rds'))
 
 
-City_URL = paste0('ftp://wakeftp.co.wake.nc.us/gis/Webdownloads/',
+city_URL = paste0('ftp://wakeftp.co.wake.nc.us/gis/Webdownloads/',
                   'SHAPEFILES/Wake_Jurisdictions_2020_10.zip')
-Municipality = url_shp_to_sf(City_URL)
+municipality = url_shp_to_sf(city_URL)
 out_path     = "./data/Municipal_October_2020"
-write_sf(Municipality, paste0(out_path, '.shp'))
-write_rds(Municipality, paste0(out_path, '.rds'))
+write_sf(municipality, paste0(out_path, '.shp'))
+write_rds(municipality, paste0(out_path, '.rds'))
+
+wake_url = paste0('ftp://wakeftp.co.wake.nc.us/gis/Webdownloads/',
+                  'SHAPEFILES/Wake_Zoning_2020_10.zip')
+wake = url_shp_to_sf(wake_url)
+wake =st_sf(st_union(wake))
+out_path     = "./data/Wake_October_2020"
+write_sf(municipality, paste0(out_path, '.shp'))
+write_rds(municipality, paste0(out_path, '.rds'))
+
--- scripts/02_Initial_Analysis.R
+++ scripts/02_Initial_Analysis.R
@@ -4,9 +4,10 @@ source('./scripts/SetUp.R', echo = TRUE)
 # Extracting Vacant -------------------------------------------------------
 parcels            <- read_rds("./data/Parcels_October_2020.rds")
 parcels$area       <- as.numeric(st_area(parcels))
-parcels            <- st_centroid(parcels)
-vacant_parcels     <- subset(parcels, parcels$LAND_CLASS == "VAC")
-non_vacant_parcels <- subset(parcels, parcels$LAND_CLASS != "VAC")
+vacant_parcels     <- subset(st_centroid(parcels), 
+                             parcels$LAND_CLASS == "VAC")
+non_vacant_parcels <- subset(st_centroid(parcels), 
+                             parcels$LAND_CLASS != "VAC")
 
 non_vacant_parcels$landuse <- if_else(non_vacant_parcels$SITE == 6100,
                                       true  = "Residential",
@@ -19,12 +20,54 @@ exp_lu_join <- st_join(vacant_parcels,
                        dist = 1000, 
                        suffix = c("", "_org")
                        ) %>%
-               st_drop_geometry()
+               st_drop_geometry() %>%
+               write_rds("./data/vacant_join_October_2020.rds")
 
-write_rds(exp_lu_join, "./data/vacant_join_October_2020.rds")
+exp_lu_join <- 
+  exp_lu_join %>%
+    group_by(PIN_NUM, landuse) %>%
+    summarise(landuse_sqft = sum(area_org)) %>%
+    group_by(PIN_NUM) %>%
+    arrange(desc(landuse_sqft)) %>%
+    summarise(exp_land_use = first(landuse)) 
 
+vacant_parcels <- inner_join(parcels, exp_lu_join)
 
+# Joining Vacant to Municipalities and Agg --------------------------------
+municipalities <- read_rds("./data/Municipal_October_2020.rds")
 
+final_parcels <- st_join(vacant_parcels[c("geometry", "area", "exp_land_use")],
+                         municipalities[c("JURISDICTI", "geometry")], 
+                         join = st_within,
+                         left = TRUE)
 
+final_data <- final_parcels %>%
+  st_drop_geometry() %>% 
+  mutate(JURISDICTI = replace_na(JURISDICTI, "UNINCORPORATED")) %>%
+  filter(exp_land_use == "Residential") %>%
+  group_by(JURISDICTI) %>%
+  summarise(affordable_area = sum(area)) %>%
+  mutate(time = Sys.Date()) %>% 
+  write_csv("./data/Affordable_Area_by_Town.csv")
 
+wake_co <- read_sf("./data/Wake_Zoning_2020_10.shp") %>% 
+  st_union() %>% 
+  st_sf() %>% 
+  transmute(ACRES      = NA,
+            JURISDICTI = "Wake County",
+            PLAN_JURIS = NA,
+            CREATED_US = NA,
+            CREATED_DA = NA ,
+            LAST_EDITE = NA,
+            LAST_EDI_1 = NA,
+            SHAPE_AREA = NA,
+            SHAPE_LEN  = NA)
 
+map_data <- rbind(municipalities, wake_co) %>% 
+  left_join(final_data) %>%
+  mutate(JURISDICTI = if_else(JURISDICTI == "RDU" | JURISDICTI == "RTP",
+                              JURISDICTI, 
+                              str_to_title(JURISDICTI))) %>%
+  write_rds("./data/map_data.rds")
+ 
+                         
